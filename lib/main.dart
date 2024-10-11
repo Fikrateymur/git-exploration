4ab35a6161407d1f0293c7f9bc44959fee97b1ae
Fikrateymur <teymurov.fikrat@gmail.com>
2024-10-11 22:56:16 +0400

    Logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tody_app/core/app_colors.dart';
 import 'package:tody_app/core/app_typhography.dart';
 import 'package:tody_app/core/constants/routes.dart';
@@ -6,68 +7,85 @@ import 'package:tody_app/presentation/pages/home_page.dart';
 import 'package:tody_app/presentation/pages/login_page.dart';
 import 'package:tody_app/presentation/pages/onboarding_page.dart';
 import 'package:tody_app/presentation/pages/splash_page.dart';
-// import 'package:tody_app/presentation/pages/splash_page.dart';
-// import 'package:tody_app/test/my_page.dart';
+import 'package:tody_app/presentation/settings/settings_scope.dart';
+import 'package:tody_app/presentation/settings/settings_scope_widget.dart';
 
-void main() {
-  runApp(const MyApp());
+// final themeNotifier = ValueNotifier<ThemeMode>(ThemeMode.light);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final preferences = await SharedPreferences.getInstance();
+
+  runApp(
+    SettingsScopeWidget(
+      preferences: preferences,
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        textTheme: TextTheme(
-          displaySmall: AppTyphography.displaySmall.w400.copyWith(
-            color: AppColors.onSurface,
-          ),
-          titleMedium: AppTyphography.titleMedium.w500.copyWith(
-            color: AppColors.pimaryVariant,
-          ),
-        ),
-        scaffoldBackgroundColor: AppColors.surface,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.primary,
-            textStyle: AppTyphography.labelLarge.w500.copyWith(
-              color: AppColors.onPrimary,
+    return Builder(
+      builder: (context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          themeMode: SettingsScope.of(context)?.themeMode,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            textTheme: TextTheme(
+              displaySmall: AppTyphography.displaySmall.w400.copyWith(
+                color: AppColors.onSurface,
+              ),
+              titleMedium: AppTyphography.titleMedium.w500.copyWith(
+                color: AppColors.pimaryVariant,
+              ),
+            ),
+            scaffoldBackgroundColor: AppColors.surface,
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primary,
+                textStyle: AppTyphography.labelLarge.w500.copyWith(
+                  color: AppColors.onPrimary,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        textTheme: TextTheme(
-          displaySmall: AppTyphography.displaySmall.w400.copyWith(
-            color: DarkAppColors.onSurface,
-          ),
-          titleMedium: AppTyphography.titleMedium.w500.copyWith(
-            color: DarkAppColors.pimaryVariant,
-          ),
-        ),
-        scaffoldBackgroundColor: DarkAppColors.surface,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: DarkAppColors.primary,
-            textStyle: AppTyphography.labelLarge.w500.copyWith(
-              color: AppColors.onPrimary,
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            textTheme: TextTheme(
+              displaySmall: AppTyphography.displaySmall.w400.copyWith(
+                color: DarkAppColors.onSurface,
+              ),
+              titleMedium: AppTyphography.titleMedium.w500.copyWith(
+                color: DarkAppColors.pimaryVariant,
+              ),
+            ),
+            scaffoldBackgroundColor: DarkAppColors.surface,
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: DarkAppColors.primary,
+                textStyle: AppTyphography.labelLarge.w500.copyWith(
+                  color: AppColors.onPrimary,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      initialRoute: EnumRoutes.splash.path,
-      routes: {
-        EnumRoutes.splash.path: (context) => const SplashPage(),
-        EnumRoutes.onboarding.path: (context) => const OnboardingPage(),
-        EnumRoutes.login.path: (context) => const LoginPage(),
-        EnumRoutes.home.path: (context) => const HomePage(),
+          initialRoute: EnumRoutes.splash.path,
+          routes: {
+            EnumRoutes.splash.path: (context) => const SplashPage(),
+            EnumRoutes.onboarding.path: (context) => const OnboardingPage(),
+            EnumRoutes.login.path: (context) => const LoginPage(),
+            EnumRoutes.home.path: (context) => const HomePage(),
+          },
+        );
       },
     );
   }
